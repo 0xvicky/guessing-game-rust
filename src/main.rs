aa50cc0b6e181f4d8d1628385b313fc681ef565b
vicky <tyagivivek528@gmail.com>
2023-06-13 23:51:49 +0530

    random number generator dependency integrated

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
 use std::io;//to use input output library
+use rand::Rng;
 
 fn main() {
     println!("Hello, world!");
 
-    println!("Input variables:");
+    println!("Enter your guess:");
+    let secret_number = rand::thread_rng().gen_range(1,101);
+    let mut guess = String::new(); //In rust, by default variables are immutable so we use "mut" keyword
 
-    let mut guess = String::new()://In rust, by default variables are immutable so we use "mut" keyword
+    io::stdin()
+    .read_line(&mut guess)
+    .expect("Failed to read msg.");//Here stdin give us handle the input, Also "read_line" takes the input and append it to the buffer, buffer here in this case is guess variable, ultimately the guess is assigned a value
 
-    io::stdin().read_line(buf:&mut guess)
+    println!("You guess: {}", guess);
+
+    
+    //Now we need to generate random numbers, so we need dependency
+
 }
+
+
 
